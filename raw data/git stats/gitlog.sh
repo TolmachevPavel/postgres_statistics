5c493c9c12fd16170d7ee542b3288ac7c2253e09
Pavel Tolmachev <tolmachev.pavel@gmail.com>
2024-04-06 18:20:04 +0300

    Немного подправил скрипт сбора инфы о патчах

--- raw data/git stats/gitlog.sh
+++ raw data/git stats/gitlog.sh
@@ -3,26 +3,26 @@
 # Путь до директории с исходным кодом PostgreSQL
 cd /home/pavel/postgresql
 
-echo По месяцам:
+echo По месяцам 2019-2024
 git log --since='2019-03-27' --until='2024-03-27' --format='%ad' --date='format:%m' master | sort | uniq -c
 
-echo По дням:
+echo По дням 2019-2024:
 git log --since='2019-03-27' --until='2024-03-27' --format='%ad' --date='format:%d' master | sort | uniq -c
 
-echo 2019-2024
+echo Патчи за 2019-2024
 git log --since='2019-03-27' --until='2024-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
 
-echo 2014-2019
+echo Патчи за 2014-2019
 git log --since='2014-03-27' --until='2019-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
 
-echo 2009-2014
+echo Патчи за 2009-2014
 git log --since='2009-03-27' --until='2014-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
 
-echo 2004-2009
+echo Патчи за 2004-2009
 git log --since='2004-03-27' --until='2009-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
 
-echo 1999-2004
+echo Патчи за 1999-2004
 git log --since='1999-03-27' --until='2004-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
 
-echo 1996-1999
+echo Патчи за 1996-1999
 git log --until='1999-03-27' --format='%ad' --date='format:%a' master | sort | uniq -c
